1d30958172a17ca3ae1be2a33e092342e87da856
daso20 <dnlsuarez97@gmail.com>
2024-09-16 17:05:30 +0200

    Added additional methods and unit tests

--- app/methods.py
+++ app/methods.py
@@ -9,15 +9,39 @@ def GetPlanet(planet_name):
     with Session(engine) as session:
         stmt = select(Planets).where(Planets.name.in_([planet_name]))
         planet = session.scalars(stmt).first()
+        if planet == None:
+            print("Planet could not be found in database")
         return planet
 
-def AddPlanet(planet_name):
-    pass
+## "planet_parameters" needs to be entered as the following:
+## ['Earth', '1', '1', '1', '1', '7.25', '0.017', '1', '1', '23.44', 'no', 'N2, O2, Ar']
+def AddPlanet(planet_parameters, planetary_system_id):
+    with Session(engine) as session:
+        dict_of_parameters = {}
+        i = 0
+        for parameter in planet_parameters:
+            dict_of_parameters[parameter] = planet_parameters[i]
+            i += 1
+        dict_of_parameters['planetary_system_id'] = planetary_system_id
+        session_object = Planets(**dict_of_parameters)
+        session.add(session_object)
+        session.commit()
+
+        return None
 
 ## Add planets from CSV
 
-def UpdatePlanet(planet_name):
-    pass
+def UpdatePlanet(planet_name, parameter, value):
+    with Session(engine) as session:
+        stmt = select(Planets).where(Planets.name == planet_name)
+        planet = session.scalars(stmt).one()
+
+        if not hasattr(planet, parameter):
+            raise AttributeError(f"'{parameter}' is not a valid attribute of Planets")
+        else:
+            setattr(planet, parameter, value)
+            session.commit()
+        return planet
 
 def DeletePlanet(planet_name):
     pass
--- app/test.py
+++ app/test.py
@@ -1,5 +1,11 @@
-from methods import GetPlanet
+from methods import GetPlanet, AddPlanet, UpdatePlanet
 
 def test_Get():
     result = GetPlanet('Venus')
     assert result.attributes_as_list() == ['Venus', 1, '0.949', '0.82', '0.72', '0.62', '3.86', '0.007', '-243.02', '0', '177.36', 'no', 'CO2, N2']
+
+def test_Update():
+    UpdatePlanet('Venus', 'mass', '2.82') # Original = 0.82
+    result = GetPlanet('Venus')
+    assert result.mass == "2.82"
+    UpdatePlanet('Venus', 'mass', '0.82')
