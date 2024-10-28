924a2305f847809d0c12c971f81f0c3978f7d8d0
daso20 <dnlsuarez97@gmail.com>
2024-10-28 13:00:02 +0100

    requirements.txt updated and database created if it doesn't exists

--- app/connection.py
+++ app/connection.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
 from config import settings
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if not database_exists(engine.url):
+    create_database(engine.url)
